4de72f9b7e9f54d31e47dc2e94ba0d433fcfdaa6
Bartcardi <bart.joosten@gmail.com>
2023-03-29 13:52:55 +0200

    Add status and state checks to control flow of heos grouping

--- appdaemon/apps/heos.py
+++ appdaemon/apps/heos.py
@@ -7,130 +7,151 @@ from pyheos import Heos
 class ControlHeos(hass.Hass):
     def initialize(self):
         self.listen_state(
-            # self.ungroup, "media_player.lg_webos_tv_oled55g1rla", new="on"
-            self.ungroup,
-            "media_player.lg_webos_tv_f6ee",
-            new="on",
+            self.ungroup, "media_player.lg_webos_tv_oled55g1rla", new="on"
         )
-        # self.listen_state(self.ungroup, "light.scissor_light_light", new="on")
-        # self.listen_state(self.group, "media_player.lg_webos_tv_oled55g1rla", new="off")
-        self.listen_state(self.group, "media_player.lg_webos_tv_f6ee", new="off")
-        # self.listen_state(self.group, "light.scissor_light_light", new="off")
+        self.listen_state(self.ungroup, "input_button.heos_ungroup")
+        self.listen_state(self.group, "media_player.lg_webos_tv_oled55g1rla", new="off")
+        self.listen_state(self.group, "input_button.heos_group")
 
     async def ungroup(self, entity, attribute, old, new, kwargs):
-        response = sh.shoe("-H", "192.168.179.193", "-c", "GetGroupUUID")
-        self.log(response)
-        matches = re.findall(r":\s.*", response)
+        heos_control = self.get_entity("input_boolean.heos_is_being_setup")
+        heos_controlled = await heos_control.is_state("on")
+        heos_ungroup = self.get_entity("input_boolean.heos_ungrouped")
+        heos_ungrouped = await heos_ungroup.is_state("on")
+        if not (heos_controlled or heos_ungrouped):
+            self.call_service(
+                "input_boolean/turn_on", entity_id="input_boolean.heos_is_being_setup"
+            )
 
-        if matches:
-            groupUUID = matches[-1][2:]
-            self.log(f"groupUUID is {groupUUID}")
+            response = sh.shoe("-H", "192.168.179.193", "-c", "GetGroupUUID")
+            self.log(response)
+            matches = re.findall(r":\s.*", response)
+
+            if matches:
+                groupUUID = matches[-1][2:]
+                self.log(f"groupUUID is {groupUUID}")
+
+                #  First stop playback if playing
+                response = sh.shoe("-H", "192.168.179.193", "-c", "Stop")
+                self.log(f"Stop response right is {response}")
+                response = sh.shoe("-H", "192.168.179.178", "-c", "Stop")
+                self.log(f"Stop response left is {response}")
+
+                response = sh.shoe(
+                    "-H",
+                    "192.168.179.193",
+                    "-c",
+                    "DestroyGroup",
+                    "-a",
+                    "GroupUUID",
+                    groupUUID,
+                )
+                self.log(f"Destroy response is {response}")
+
+            self.call_service(
+                "media_player/select_source",
+                source="Denon Home right rear - AUX In",
+                entity_id=[
+                    "media_player.denon_home_right_rear",
+                ],
+            )
+
+            self.call_service(
+                "media_player/volume_set",
+                entity_id="media_player.denon_home_right_rear",
+                volume_level=0.5,
+            )
+
+            response = sh.shoe("-H", "192.168.179.193", "-c", "Play")
+            self.log(f"Play response right is {response}")
+
+            # This Heos device probably is not available after the unjoining so
+            # let's call it from pyheos (maybe later by reloading integration)
+            heos = Heos("192.168.179.193")
+
+            await heos.connect(auto_reconnect=True)
+            players = await heos.get_players(refresh=True)
+
+            heos_left = players[751373395]
+            await heos_left.play_input("inputs/aux_in_1")
+            await heos_left.set_volume(50)
+            await heos.disconnect()
+
+            self.call_service(
+                "input_boolean/turn_off", entity_id="input_boolean.heos_is_being_setup"
+            )
+            self.call_service(
+                "input_boolean/turn_on", entity_id="input_boolean.heos_ungrouped"
+            )
+            self.call_service(
+                "input_boolean/turn_off", entity_id="input_boolean.heos_grouped"
+            )
+
+    def group(self, entity, attribute, old, new, kwargs):
+        heos_control = self.get_entity("input_boolean.heos_is_being_setup")
+        heos_controlled = heos_control.is_state("on")
+        heos_group = self.get_entity("input_boolean.heos_grouped")
+        heos_grouped = heos_group.is_state("on")
+        if not (heos_controlled or heos_grouped):
+
+            self.call_service(
+                "input_boolean/turn_on", entity_id="input_boolean.heos_is_being_setup"
+            )
+
+            self.call_service(
+                "media_player/volume_set",
+                entity_id="media_player.denon_home_right_rear",
+                volume_level=0.2,
+            )
+
+            self.call_service(
+                "media_player/volume_set",
+                entity_id="media_player.denon_home_left_rear",
+                volume_level=0.2,
+            )
 
             #  First stop playback if playing
             response = sh.shoe("-H", "192.168.179.193", "-c", "Stop")
-            self.log(f"Stop resonse right is {response}")
+            self.log(f"Stop response right is {response}")
             response = sh.shoe("-H", "192.168.179.178", "-c", "Stop")
-            self.log(f"Stop resonse left is {response}")
+            self.log(f"Stop response left is {response}")
 
             response = sh.shoe(
                 "-H",
                 "192.168.179.193",
                 "-c",
-                "DestroyGroup",
+                "CreateZone",
                 "-a",
-                "GroupUUID",
-                groupUUID,
+                "ZoneFriendlyName",
+                "Danger Zone",
+                "-a",
+                "ZoneIPList",
+                "192.168.179.178",
             )
-            self.log(f"Destroy resonse is {response}")
+            self.log(response)
 
-        self.call_service(
-            "media_player/select_source",
-            source="Denon Home right rear - AUX In",
-            entity_id=[
-                "media_player.denon_home_right_rear",
-            ],
-        )
-        
-        self.call_service(
-            "media_player/volume_set",
-            entity_id="media_player.denon_home_right_rear",
-            volume_level=0.5,
-        )
+            response = sh.shoe(
+                "-H",
+                "192.168.179.193",
+                "-c",
+                "CreateGroup",
+                "-a",
+                "GroupFriendlyName",
+                "Stereo Friends",
+                "-a",
+                "GroupMemberUUIDList",
+                "11b9df62864e1a53008000a96f10e920,6bcb52d849471444008000a96f107e22",
+            )
+            self.log(response)
 
-        response = sh.shoe("-H", "192.168.179.193", "-c", "Play")
-        self.log(f"Play resonse right is {response}")
+            self.call_service(
+                "input_boolean/turn_off", entity_id="input_boolean.heos_is_being_setup"
+            )
 
-        # This Heos device probably is not available after the unjoining so 
-        # let's call it from pyheos (maybe later by reloading integration)
-        # self.call_service(
-        #     "media_player/select_source",
-        #     source="Denon Home left rear - AUX In",
-        #     entity_id=[
-        #         "media_player.denon_home_left_rear",
-        #     ],
-        # )
-        
-        heos = Heos('192.168.179.193')
+            self.call_service(
+                "input_boolean/turn_on", entity_id="input_boolean.heos_grouped"
+            )
 
-        await heos.connect(auto_reconnect=True)
-        players = await heos.get_players(refresh=True)
-
-        heos_left = players[751373395]
-        await heos_left.play_input('inputs/aux_in_1')
-        await heos_left.set_volume(50)
-        await heos.disconnect()
-
-        response = sh.shoe("-H", "192.168.179.178", "-c", "Play")
-        self.log(f"Play resonse left is {response}")
-
-
-
-
-    def group(self, entity, attribute, old, new, kwargs):
-
-        self.call_service(
-            "media_player/volume_set",
-            entity_id="media_player.denon_home_right_rear",
-            volume_level=0.2,
-        )
-
-        self.call_service(
-            "media_player/volume_set",
-            entity_id="media_player.denon_home_left_rear",
-            volume_level=0.2,
-        )
-
-        #  First stop playback if playing
-        response = sh.shoe("-H", "192.168.179.193", "-c", "Stop")
-        self.log(f"Stop resonse right is {response}")
-        response = sh.shoe("-H", "192.168.179.178", "-c", "Stop")
-        self.log(f"Stop resonse left is {response}")
-
-        response = sh.shoe(
-            "-H",
-            "192.168.179.193",
-            "-c",
-            "CreateZone",
-            "-a",
-            "ZoneFriendlyName",
-            "Danger Zone",
-            "-a",
-            "ZoneIPList",
-            "192.168.179.178",
-        )
-        self.log(response)
-
-        response = sh.shoe(
-            "-H",
-            "192.168.179.193",
-            "-c",
-            "CreateGroup",
-            "-a",
-            "GroupFriendlyName",
-            "Stereo Friends",
-            "-a",
-            "GroupMemberUUIDList",
-            "11b9df62864e1a53008000a96f10e920,6bcb52d849471444008000a96f107e22",
-        )
-        self.log(response)
-
+            self.call_service(
+                "input_boolean/turn_off", entity_id="input_boolean.heos_ungrouped"
+            )
